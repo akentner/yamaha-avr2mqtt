1b20d4659cedd2fa60fe9a66a60bd037f7afe8de
Alexander Kentner <github@akentner.de>
2015-12-29 10:01:46 +0100

    better run

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-echo src/app.js -m $MQTT_HOST -t $MQTT_TOPIC -q $MQTT_QOS -y $YAMAHA_HOST
+echo node src/app.js -m $MQTT_HOST -t $MQTT_TOPIC -q $MQTT_QOS -y $YAMAHA_HOST
 node -v
 node src/app.js -m $MQTT_HOST -t $MQTT_TOPIC -q $MQTT_QOS -y $YAMAHA_HOST
