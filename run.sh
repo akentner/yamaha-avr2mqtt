259bcb9e7f84c5d7042ad372abad7f09a7c2e6f8
Alexander Kentner <github@akentner.de>
2015-12-29 11:01:34 +0100

    fix npm

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 echo node src/app.js -m $MQTT_HOST -t $MQTT_TOPIC -q $MQTT_QOS -y $YAMAHA_HOST
 node -v
+npm i
 node src/app.js -m $MQTT_HOST -t $MQTT_TOPIC -q $MQTT_QOS -y $YAMAHA_HOST
